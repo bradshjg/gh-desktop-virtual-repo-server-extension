c25fff7fb5983bb660e25b56b13498606f30cb1b
Jimmy Bradshaw <james.g.bradshaw@gmail.com>
2021-08-08 22:31:25 +0000

    implement start/stop

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as http from 'http';
 
 import { app } from './server';
 
+let server: (null | http.Server) = null;
+const PORT = 9195;
 
-export function activate() {
+export function activate(context: vscode.ExtensionContext) {
+	let startDisposable = vscode.commands.registerCommand('github-desktop-virtual-server.startServer', async () => {
+		if (server instanceof http.Server) {
+			return
+		}
+		server = app.listen(PORT);
+		vscode.env.asExternalUri(vscode.Uri.parse(`http://localhost:${PORT}`));
+		context.subscriptions.push(startDisposable);
+	});
 
-	const PORT = 9195;
-
-	console.log(`github-desktop-virtual-server is now active! Binding to and forwarding ${PORT}!`);
-
-	app.listen(PORT);
-	vscode.env.asExternalUri(vscode.Uri.parse(`http://localhost:${PORT}`));
+	let stopDisposable = vscode.commands.registerCommand('github-desktop-virtual-server.stopServer', async () => {
+		if (server instanceof http.Server) {
+			server.close();
+		}
+		vscode.window.showInformationMessage(`Deactivate port ${PORT} in the "Ports" tab to complete cleanup.`);
+		context.subscriptions.push(stopDisposable);
+	});	
 }
 
-// TODO I _think_ VS code can handle cleanup, but we'll see
-export function deactivate() {}
+export function deactivate() {
+	if (server instanceof http.Server) {
+		server.close();
+	}
+}
